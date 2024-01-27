f62407b59da087b618b507613b1dd2ee5c6b918b
dhev-wisdom <nonsowisdom62@gmail.com>
2024-01-27 14:14:11 +0100

    check

--- dashboard.js
+++ dashboard.js
@@ -103,42 +103,53 @@ platformRange.forEach((element, index) => {
     });
 });
 
-// const ctx = document.getElementById('myChart');
-// const monthNames = [
-//     "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-//     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-// ];
+const ctx = document.getElementById('myChart');
+const monthNames = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
 
-// const amount = [7000, 21000, 3600, 28000, 9000, 45000,
-// 9100, 18000, 35000, 4500, 30000, 24000];
-// displayMainGraph(amount);
+const amount = [7000, 21000, 3600, 28000, 9000, 45000,
+9100, 18000, 35000, 4500, 30000, 24000];
+displayMainGraph(amount);
      
-// function displayMainGraph(amounts) {
-//     new Chart(ctx, {
-//         type: 'bar',
-//         data: {
-//         labels: monthNames,
-//         datasets: [{
-//             label: 'Sales Trend',
-//             data: amounts,
-//             borderWidth: 1,
-//             borderRadius: 50,
-//             backgroundColor: (context) => {const lightModeBtn = document.getElementById("light-mode");
-//             const index = context.dataIndex;
-//             const data = context.dataset.data;
-//             context.dataset.barPercentage = .85;
-//             if (index === data.length - 7) return 'rgb(91, 206, 170)';
-//             else return 'rgb(235, 250, 246)';
-//             },
-//         }]
-//         },
-//         options: {
-//         scales: {
-//             y: {
-//             beginAtZero: true,
-//             suggestedMax: 50000,
-//             }
-//         }
-//         }
-//     });
-// }
+function displayMainGraph(amounts) {
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: monthNames,
+        datasets: [{
+            label: 'Sales Trend',
+            data: amounts,
+            borderWidth: 1,
+            borderRadius: 50,
+            // backgroundColor: (context) => {const lightModeBtn = document.getElementById("light-mode");
+            // const index = context.dataIndex;
+            // const data = context.dataset.data;
+            // context.dataset.barPercentage = .85;
+            // if (index === data.length - 7) return 'rgb(91, 206, 170)';
+            // else return 'rgb(235, 250, 246)';
+            // },
+            backgroundColor: "rgb(235, 250, 246)",
+            // hoverBackgroundColor: "linear-gradient(to top, rgb(235, 250, 246), rgb(91, 206, 170))",
+            hoverBackgroundColor: "rgb(91, 206, 170)",
+
+        }]
+        },
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true,
+            suggestedMax: 50000,
+            }
+        }
+        }
+    });
+}
+
+function hideLoadingOverlay() {
+    document.getElementById('loadingOverlay').style.display = 'none';
+}
+setTimeout(() => {
+    hideLoadingOverlay();
+}, 1000);
